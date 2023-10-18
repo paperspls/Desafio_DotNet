bb2e98dbab7986419d7663cc76042ba5ce29b523
Rhyan Magalhães <rjobdg@outlook.com>
2023-10-18 02:14:13 -0300

    Configurando o ambiente do projeto

---
+++ Geo_WebApi_ASP.NET/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Geo_WebApi_ASP.NET.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            { 
+
+            }
+    }
+}
